b7f9fe4b85bc915da5eeb56e906ebb7051f69ced
nthai21 <nnthai.dhkl17a2hn@sv.uneti.edu.vn>
2023-10-30 15:51:18 +0700

    int project

---
+++ Baitap_thu6.py
@@ -0,0 +1,27 @@
+#Bài1.1
+print("**    **  ******  **        **        ******")
+print("**    **  **      **        **        **  **")
+print("********  ******  **        **        **  **")
+print("**    **  **      **        **        **  **")
+print("**    **  ******  ******    ******    ******")
+#............................................The and...................................................
+#Bài 1.2
+x=float(input ("Nhap gia tri x="))
+y=float(input ("Nhap gia tri y="))
+tong = x+y
+hieu = x-y
+tich = x*y
+thuong =x/y
+print ("tong cua hai so ",x,"+" , y ,"=", tong)
+print ("hieu cua hai so ",x,"-" , y ,"=", hieu)
+print ("tich cua hai so ",x,"*" , y ,"=", tich)
+print ("thuong cua hai so ",x,"/",y,"=",thuong)
+#.............................................The and....................................................
+#Bài1.3
+print("Sữa hộp Vina Milk")
+a=float(input("Nhập vào số lượng sữa:"))
+b=float(input("Đơn giá sữa hôp:"))
+tien= a*b
+print ("Số tiền cần trả là:",tien)
+#.............................................The and....................................................
+
